1a7f2387446a803579676a4b31c4f555136692bf
halfdev <gusgodnd@naver.com>
2020-03-24 00:02:29 +0900

    # 4 AdminController Done (Todo, Junit)

--- src/main/java/org/open/market/web/AdminAccountController.java
+++ src/main/java/org/open/market/web/AdminAccountController.java
@@ -3,16 +3,22 @@ package org.open.market.web;
 import lombok.RequiredArgsConstructor;
 import org.open.market.model.accounts.Account;
 import org.open.market.model.accounts.AccountStatus;
+import org.open.market.model.dto.AccountDto;
 import org.open.market.service.AccountService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedResources;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
+
+/**
+* @author halfdev
+* @since 2020-03-23
+* Admin 전용 Page
+*/
 @Controller
 @RequestMapping("/admin/accounts")
 @RequiredArgsConstructor
@@ -27,6 +33,39 @@ public class AdminAccountController {
 
         return ResponseEntity.ok(pagedResources);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getAccount(@PathVariable("id") Long id) {
+        AccountDto accountDto;
+
+        try {
+            accountDto = accountService.getAccount(id);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(accountDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteAccount(@PathVariable("id") Long id,
+                                        @RequestParam AccountStatus accountStatus) {
+        AccountDto accountDto;
+
+        try {
+            accountDto = accountService.getAccount(id);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+
+        if (accountStatus.equals(AccountStatus.DELETED)) {
+            return ResponseEntity.badRequest().build(); // ADMIN 이 DELETED 로 불가능
+        }
+        accountService.deleteAccountStatus(id, accountStatus);
+
+        return ResponseEntity.ok(accountService.getAccount(accountDto.getId()));
+    }
+
 
 }
 
